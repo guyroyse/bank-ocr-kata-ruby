3af1261d269d5746af7e1a17d0ac93351d492d8e
Guy Royse <guy@guyroyse.com>
2012-02-15 07:39:22 -0500

    Fixed a small rogue tile in human readable portion of specs

--- spec/ocr_spec.rb
+++ spec/ocr_spec.rb
@@ -11,39 +11,39 @@ describe Ocr do
     check_account_number '000000000'
   end
 
-  it "parses and account number of all 1s" do
+  it "parses an account number of all 1s" do
     check_account_number '111111111'
   end
 
-  it "parses and account number of all 2s" do
+  it "parses an account number of all 2s" do
     check_account_number '222222222'
   end
 
-  it "parses and account number of all 3s" do
+  it "parses an account number of all 3s" do
     check_account_number '333333333'
   end
 
-  it "parses and account number of all 4s" do
+  it "parses an account number of all 4s" do
     check_account_number '444444444'
   end
 
-  it "parses and account number of all 5s" do
+  it "parses an account number of all 5s" do
     check_account_number '555555555'
   end
 
-  it "parses and account number of all 6s" do
+  it "parses an account number of all 6s" do
     check_account_number '666666666'
   end
 
-  it "parses and account number of all 7s" do
+  it "parses an account number of all 7s" do
     check_account_number '777777777'
   end
 
-  it "parses and account number of all 8s" do
+  it "parses an account number of all 8s" do
     check_account_number '888888888'
   end
 
-  it "parses and account number of all 9s" do
+  it "parses an account number of all 9s" do
     check_account_number '999999999'
   end
 
