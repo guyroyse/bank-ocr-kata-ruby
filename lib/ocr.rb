8df01ef8f8d7c15ce19b6146390de7461a83cc25
Guy Royse <guy@guyroyse.com>
2012-02-22 07:09:17 -0500

    extracted digit to own class and renamed a couple variables

--- lib/ocr.rb
+++ lib/ocr.rb
@@ -34,29 +34,34 @@ class Ocr
 end
 
 class Account
-  attr_reader :number
   def initialize text
-      @scanned = text
-      @number = extract_account_number
+      @text = text
   end
-  def extract_account_number
-    (0...ACCOUNT_NUMBER_LENGTH).reduce '' do |account, digit_position|
-      digit = extract_digit digit_position
-      account << lookup_number(digit)
+  def number
+    (0...ACCOUNT_NUMBER_LENGTH).reduce '' do |account_number, digit_position|
+      digit_text = extract_digit_text digit_position
+      account_number << Digit.new(digit_text).value
     end
   end
-  def extract_digit digit_position
+  def extract_digit_text digit_position
     (0...DIGIT_HEIGHT).reduce ''  do |digit, line|
       offset = calculate_offset line, digit_position
-      digit << @scanned[offset, DIGIT_WIDTH]
+      digit << @text[offset, DIGIT_WIDTH]
     end
-  end
-  def lookup_number digit
-    $digits[digit]
   end
   def calculate_offset line, digit_position
     line_offset = line * LINE_LENGTH
     digit_offset = digit_position * DIGIT_WIDTH
     offset = line_offset + digit_offset
+  end
+end
+
+class Digit
+  attr_reader :value
+  def initialize text
+    @value = lookup_number text
+  end
+  def lookup_number text
+    $digits[text]
   end
 end
