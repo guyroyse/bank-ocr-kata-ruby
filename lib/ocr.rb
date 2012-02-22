18c7fbfd9b1eae936d563ed437e4f1a5b45b3c60
Guy Royse <guy@guyroyse.com>
2012-02-22 06:58:53 -0500

    extracted account number parsing to own class

--- lib/ocr.rb
+++ lib/ocr.rb
@@ -14,37 +14,49 @@ class Ocr
   def scan file
     read_file file
     (0...count_account_numbers).each do |account|
-      @accounts << extract_account_number(account)
+      account_text = extract_account_text account
+      @accounts << Account.new(account_text).number
     end
   end
   def read_file file
     File.open file do |file| @scanned = file.read end
   end
-  def extract_account_number acct_position
-    (0...ACCOUNT_NUMBER_LENGTH).reduce '' do |account, digit_position|
-      digit = extract_digit acct_position, digit_position
-      account << lookup_number(digit)
-    end
-  end
   def count_account_numbers
     count_lines / 4
   end
-  def extract_digit acct_position, digit_position
+  def count_lines
+    lines = @scanned.count "\n"
+  end
+  def extract_account_text acct_position
+    acct_offset = acct_position * ACCT_LENGTH
+    @scanned[acct_offset, ACCT_LENGTH]
+  end
+end
+
+class Account
+  attr_reader :number
+  def initialize text
+      @scanned = text
+      @number = extract_account_number
+  end
+  def extract_account_number
+    (0...ACCOUNT_NUMBER_LENGTH).reduce '' do |account, digit_position|
+      digit = extract_digit digit_position
+      account << lookup_number(digit)
+    end
+  end
+  def extract_digit digit_position
     (0...DIGIT_HEIGHT).reduce ''  do |digit, line|
-      offset = calculate_offset acct_position, line, digit_position
+      offset = calculate_offset line, digit_position
       digit << @scanned[offset, DIGIT_WIDTH]
     end
   end
   def lookup_number digit
     $digits[digit]
   end
-  def count_lines
-    lines = @scanned.count "\n"
-  end
-  def calculate_offset acct_position, line, digit_position
-    acct_offset = acct_position * ACCT_LENGTH
+  def calculate_offset line, digit_position
     line_offset = line * LINE_LENGTH
     digit_offset = digit_position * DIGIT_WIDTH
-    offset = acct_offset + line_offset + digit_offset
+    offset = line_offset + digit_offset
   end
 end
